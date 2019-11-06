d75eae3e212ab11f90f3f7e08b1a1f4133d21c64
Diego Chavão <diego.chavao@corp.globo.com>
2019-11-06 11:44:12 -0300

    Add installer script

---
+++ install.sh
@@ -0,0 +1,33 @@
+if [ -e ~/.zprezto ]
+then
+  mv -f ~/.zprezto ~/.zprezto-before-chavao-prezto
+fi
+
+if [ -e ~/.zshrc ]
+then
+  mv -f ~/.zshrc ~/.zshrc-before-chavao-prezto
+fi
+
+git clone https://github.com/Chavao/prezto ~/.zprezto
+
+cd ~/.zprezto
+
+git submodule update --init --recursive
+
+cd $HOME
+
+ln -sf ~/.zprezto/runcoms/zlogin .zlogin
+ln -sf ~/.zprezto/runcoms/zlogout .zlogout
+ln -sf ~/.zprezto/runcoms/zpreztorc .zpreztorc
+ln -sf ~/.zprezto/runcoms/zprofile .zprofile
+ln -sf ~/.zprezto/runcoms/zshenv .zshenv
+
+touch ~/.zshrc
+
+cat >> ~/.zshrc << EOF
+# Load zshrc
+source $HOME/.zprezto/runcoms/zshrc
+
+# Customize to your needs...
+EOF
+
