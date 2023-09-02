63d4240959d5357d21f5ceb092c6484d5ee68fb4
Diego Chavão <fale@chavao.net>
2023-09-02 10:17:51 -0300

    Add zsh-autosuggestions plugin

--- install.sh
+++ install.sh
@@ -22,11 +22,16 @@ ln -sf ~/.zprezto/runcoms/zpreztorc .zpreztorc
 ln -sf ~/.zprezto/runcoms/zprofile .zprofile
 ln -sf ~/.zprezto/runcoms/zshenv .zshenv
 
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+
 touch ~/.zshrc
 
 cat >> ~/.zshrc << EOF
 # Load zshrc
 source $HOME/.zprezto/runcoms/zshrc
+
+# Load zsh-autosuggestions
+source $HOME/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
 
 # Customize to your needs...
 EOF
